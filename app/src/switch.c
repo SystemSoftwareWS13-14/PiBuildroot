5290e4c08813e10abaa8bf72e1b1b42497beb5e7
Fabian Meyer <rookfighter@develop>
2014-01-09 15:03:12 +0100

    counting button press! AMAZING

--- app/src/switch.c
+++ app/src/switch.c
@@ -3,19 +3,41 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#define PIN gpio4
+#include <string.h>
+#define PIN_FILE "/sys/class/gpio/gpio17/value"
 
 int main(int argc, char **argv)
 {
-	int fd = open("/sys/class/gpio/PIN/direction", O_RDONLY);
+	int fd;
+	int pressed = 0;
 	int counter = 0;
-	char buffer[2];
+	char buffer[1];
+
+	printf("Opening %s...\n", PIN_FILE);
 
 	while (1) {
-		read(fd, buffer, 2);
-		printf("Read from gpio: %d -> %s\n", ++counter, buffer);
+		fd = open(PIN_FILE, O_RDONLY);
+		if(fd < 0)
+		{
+			perror("Could not open gpio-file!");
+			return 1;
+		}
+
+		read(fd, buffer, 1);
+		if(strcmp(buffer, "0") == 0 && !pressed)
+		{
+			pressed = 1;
+			++counter;
+			printf("Button pressed! Count: %d\n", counter);
+		}
+		if(strcmp(buffer, "1") == 0 && pressed)
+		{
+			pressed = 0;
+			printf("Button released!\n");
+		}
+
+		close(fd);
 	}
 
-	close(fd);
 	return 0;
 }
